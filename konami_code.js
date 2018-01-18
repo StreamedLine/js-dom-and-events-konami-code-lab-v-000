ae57aa735103fe6914abe526eb955960aa023427
streamedLine <dovidlwtn@gmail.com>
2018-01-18 00:02:30 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,17 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let indexProg = 0;
+
+  document.body.addEventListener('keydown', function(e){
+  	const key = parseInt(e.detail || e.location || e.which);
+
+  	if (key === code[indexProg]) {
+  		indexProg++;
+  		indexProg == code.length ? alert('Good job!!') : false;
+  	} else {
+  		indexProg = 0;
+  	}
+  });
 
 }
